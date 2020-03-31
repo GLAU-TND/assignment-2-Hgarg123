c8af7510209f58c2269b1516a03ba4431521e74a
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 15:28:12 +0530

    constructor

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -11,5 +11,8 @@ public class Node {
     private Student data;
     private Node next;
 
-
+    public Node(Student data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 }
