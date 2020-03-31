1a4b57b0aa55d7c71b2b1704893a237df3a3ccd3
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 15:37:19 +0530

    method addlast

--- src/problem3/myLinkedList/MyLinkedList.java
+++ src/problem3/myLinkedList/MyLinkedList.java
@@ -1,10 +1,14 @@
 package problem3.myLinkedList;
 
 import problem3.node.Node;
+import problem3.student.Student;
 
 public class MyLinkedList {
     private Node first;
     private Node last;
 
+    public void addLast(Student student) {
+
+    }
 
 }
