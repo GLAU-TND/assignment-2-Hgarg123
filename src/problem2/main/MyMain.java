110293cf4912ba71f1d9abc017387ebaa301b762
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 20:38:36 +0530

    create object

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -6,8 +6,17 @@
  */
 package problem2.main;
 
+import problem4.binaryST.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(preQueue);
+
+    }
 
 }
