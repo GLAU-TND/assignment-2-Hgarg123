d3a05583a34315512956f93e486a34c821cb7753
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 15:45:04 +0530

    enqueue method

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -7,9 +7,16 @@
 package problem3.myqueue;
 
 import problem3.myLinkedList.MyLinkedList;
+import problem3.student.Student;
 
 public class MyPriorityQueue {
         MyLinkedList linkedList = new MyLinkedList();
+
+    public void enqueue(Student student) {
+        linkedList.addLast(student);
+    }
+
+
 
 }
 }
