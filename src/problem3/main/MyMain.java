bb663b798f74c1313ce6733b295567596cb75237
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 15:50:16 +0530

    final commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -14,12 +14,11 @@ import problem3.student.Student;
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue queue = new MyPriorityQueue();
-        queue.enqueue(new Student("Tushar", 58));
-        queue.enqueue(new Student("Tanishq", 23));
-        queue.enqueue(new Student("Pavit", 65));
-        queue.enqueue(new Student("Samar", 1));
-        queue.enqueue(new Student("Vibhu", 10));
-        queue.enqueue(new Student("Shantanu", 76));
+        queue.enqueue(new Student("Harshit", 11));
+        queue.enqueue(new Student("Garg", 12));
+        queue.enqueue(new Student("Aman", 13));
+        queue.enqueue(new Student("Hardik", 14));
+
         queue.show();
         queue.dequeue();
         queue.show();
