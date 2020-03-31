f1bc226c056085a732c5762aeb062f203bacd27b
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 20:23:30 +0530

    final commit

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -16,7 +16,15 @@ public class MyMain {
         myCircularQueue.enqueue(new Student("Y", 1, 1));
         myCircularQueue.enqueue(new Student("D", 1, 0));
 
-
+        for (int i = 0; i < myCircularQueue.getSize() + 1; i++) {
+            Student s = myCircularQueue.dequeue();
+            if (s.getRollNo() - s.getAppearingCount() != 0) {
+                myCircularQueue.enqueue(s);
+            }
+        }
+        myCircularQueue.show();
     }
+
 }
+
 
