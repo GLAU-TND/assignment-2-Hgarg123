dcd5bc53bfbbfa4069f1f075479c2b779d165344
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 20:32:56 +0530

    define fields

---
+++ src/problem2/main/BinarySearchTree.java
@@ -0,0 +1,13 @@
+package problem2.main;
+
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
+public class BinarySearchTree {
+    private TreeNode root;
+    private int countRight;
+    private MyQueue preQueue;
+    private MyQueue postQueue;
+
+
+}
--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -26,3 +26,4 @@ public class MyMain {
         myQueue.show();
     }
 }
+
