39fc8e73f788947e815825dbc915a13d3568cd8c
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 14:09:20 +0530

    complete method for traversing

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -65,4 +65,30 @@ public class MyBinarySearchTree {
         return countRight;
     }
 
+    public void setCountRight(int countRight) {
+        this.countRight = countRight;
+    }
+
+    public void traverse() {
+        TreeNode current = root;
+        TreeNode parent = null;
+        this.countRight = 0;
+        while (current != null) {
+            if (current.getLeft() == null) {
+                countRight++;
+            }
+            if (parent.getData() == current.getData()) {
+
+            }
+            if (parent.getData() < current.getData()) {
+                System.out.println(current.getData());
+                current = current.getLeft();
+            }
+            if (parent.getData() > current.getData()) {
+                current = current.getRight();
+            }
+        }
+    }
+}
+
             }
