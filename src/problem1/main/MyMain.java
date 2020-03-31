391d01ef6ba1e3cc15b4de02e99cf5829f801488
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 14:13:39 +0530

    insert elements in tree

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -12,6 +12,11 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(55);
+        binarySearchTree.insert(60);
     }
 }
 
