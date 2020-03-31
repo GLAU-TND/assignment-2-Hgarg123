69ce3ad4f528e5e3f181940982271099e9f3a4e2
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 20:22:44 +0530

    enqueue elements

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,18 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+        myCircularQueue.enqueue(new Student("T", 0, 0));
+        myCircularQueue.enqueue(new Student("Y", 1, 1));
+        myCircularQueue.enqueue(new Student("D", 1, 0));
+
+
+    }
 }
+
