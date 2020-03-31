f483cde699832def4ff55dbd4745ebb5d5a4e4d9
Hgarg123 <harshit.garg_cs18@gla.ac.in>
2020-03-31 15:40:53 +0530

    complete addlast method

--- src/problem3/myLinkedList/MyLinkedList.java
+++ src/problem3/myLinkedList/MyLinkedList.java
@@ -13,6 +13,18 @@ public class MyLinkedList {
             first = last = node;
             return;
         }
+        last.setNext(node);
+        last = node;
+        var current = first;
+        while (current != null) {
+            if (node.getData().getRollNo() < current.getData().getRollNo()) {
+                var temp = node.getData();
+                node.setData(current.getData());
+                current.setData(temp);
+            }
+            current = current.getNext();
+        }
+    }
     }
 
 }
